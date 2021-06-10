813c394d7f5d92eadc96b4d49f640973999f5712
virendrawarshetti <virendra.warshetti@gmail.com>
2021-06-10 14:58:34 +0530

    first commit

---
+++ src/main/java/com/example/demo/DockerExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerExample1Application.class, args);
+	}
+
+}
